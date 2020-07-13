008de81851a444b48ec106560ba77f2d6031b9d4
S-Vladyslav <68085487+S-Vladyslav@users.noreply.github.com>
2020-07-14 00:52:13 +0300

    Update s3.sh

--- s3.sh
+++ s3.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 pw=`pwd`
 
-if [[ ! -f "$pw/usrn" ]]
+if [[ ! -f "$pw/usrn" ]]		#adding a cron plan
 then
-	us="$USER"
+	us="$USER"			#first run the program
 	echo $USER > "usrn"
 
 	crontab -l > mycron
@@ -20,21 +20,21 @@ cnt=0
 for i in `ls /home/$us`
 do
 	cnt=0
-	if [[ ! -f $i ]]
+	if [[ ! -f $i ]]			#check directory or file
 	then
-		for j in `ls /home/$us/$i`
+		for j in `ls /home/$us/$i`	#directory
 		do
 			cnt=$(($cnt + 1))
 		done
 	else 
-		cnt="file"
+		cnt="file"			#file
 		echo "$i  $cnt" >> res1
 		continue
 	fi
 	echo "$i  $cnt" >> res2
 done
 
-sort res1 >> Desktop/res
+sort res1 >> Desktop/res		#data sorting
 sort -nk2 res2 >> Desktop/res
 rm res2
 rm res1
